57711bb1652cb80fa4cf047114d2200e7cfd4de1
Sebastian <velirt22@gmail.com>
2023-01-28 20:44:04 +0100

    fix login #8

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express';
-import { json, urlencoded } from 'body-parser';
+import pkg from 'body-parser';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
