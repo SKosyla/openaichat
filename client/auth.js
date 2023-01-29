35225a157dbdfc6327bbde9158864de03f9146e2
Sebastian <velirt22@gmail.com>
2023-01-29 06:08:17 +0100

    fix login#4

--- client/auth.js
+++ client/auth.js
@@ -7,7 +7,7 @@ class Auth {
 
 	validateAuth(auth) {
 		if (auth != 1) {
-			window.location.replace("/");
+			window.location.replace("/login");
 		} else {
             document.querySelector("body").style.display = "block";
 		}
